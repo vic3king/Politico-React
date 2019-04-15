d7bd7c39d1e4e84abc4ed604a31031b2463c1f2c
AKaniru Victory <vic3coorp@gmail.com>
2019-04-15 13:09:42 +0100

    165335379-chore(linting): implement linting with prettier and eslint - install necessary packages - create .eslintrc and prettierrc files - configure eslint airbnb style and link with prettier - add linting scripts [Finishes #165335379]

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import './index.scss';
-// import NavBar from './components/Navbar';
-import App from './components/index';
-
-// const HelloWorld = () => <h1>Hello there!</h1>;
-
-ReactDOM.render(<App />, document.getElementById('root'));
