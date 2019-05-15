bfd17059405785b93fb08634d0b4f4b06d7e572c
AKaniru Victory <vic3coorp@gmail.com>
2019-05-15 15:27:43 +0100

    165995326-feature(party): implement feature to delete a party
    - fetch delete party endpoint
    - update state after party is deleted
    
    [finishes #165995326]

--- src/components/Modals/DeletePartyModal.jsx
+++ src/components/Modals/DeletePartyModal.jsx
@@ -1,26 +1,43 @@
 import React, { Component } from 'react';
 
 class DeletePartModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   hide = e => {
     e.preventDefault();
     const { hide } = this.props;
     hide();
   };
 
+  handleDelete = e => {
+    e.preventDefault();
+    const { handleDelete } = this.props;
+    handleDelete();
+  };
+
   render() {
     return (
-      <div className="modalDelete">
-        <p>Are you sure you want to delete?</p>
-        <button type="submit" className="modalyes">
-          Yes
-        </button>
-        <button type="submit" className="modalno">
-          No
-        </button>
-        <button type="button" className="modal-close" onClick={this.hide}>
-          X
-        </button>
-      </div>
+      <React.Fragment>
+        <div className="modalDelete">
+          <p>Are you sure you want to delete?</p>
+          <button
+            onClick={this.handleDelete}
+            type="submit"
+            className="modalyes"
+          >
+            Yes
+          </button>
+          <button type="submit" className="modalno">
+            No
+          </button>
+          <button type="button" className="modal-close" onClick={this.hide}>
+            X
+          </button>
+        </div>
+      </React.Fragment>
     );
   }
 }
